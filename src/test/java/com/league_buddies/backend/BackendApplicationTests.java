eb15964de3a522f573c51574f5bdd2d1932f7410
Noel <no3lcodes@gmail.com>
2023-06-03 20:48:27 -0700

    Added starter springboot files

---
+++ src/test/java/com/league_buddies/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.league_buddies.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
